582271761ee8a6cd9058dd6e8370926f79dd77b1
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-02-06 14:30:55 -0600

    added user password area

---
+++ Front-End/JavaScript/register.js
@@ -0,0 +1,8 @@
+function showPassword() {
+    var x = document.getElementById("newPassword");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
